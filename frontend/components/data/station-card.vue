0c711b47685f671e7ffd6f6d586c54f0d0fd1945
linusX1402 <linus@wizany.at>
2025-06-06 09:37:13 +0200

    add: show and hide animation for detail

--- frontend/components/data/station-card.vue
+++ frontend/components/data/station-card.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
+import StationDetail from '~/components/data/station-detail.vue';
+
 const props = defineProps<{
   station: Station;
 }>();
 
+const isDetailOpen = ref<boolean>(false);
+
 function getWorkingChildrenCount(station: Station): number {
-  return station.children.filter((child) => child.isWorking).length;
+  return station.children.filter((child) => child.workState === 'working')
+    .length;
 }
 
 function getTotalBeansEarned(station: Station): number {
@@ -13,12 +18,17 @@ function getTotalBeansEarned(station: Station): number {
     0,
   );
 }
+function toggleDetail() {
+  isDetailOpen.value = !isDetailOpen.value;
+  document.body.style.overflow = 'hidden';
+}
 </script>
 
 <template>
   <div
-    class="flex aspect-square w-60 flex-col gap-4 rounded-xl p-6"
-    :style="'background-color:' + props.station.hexColor + 80"
+    class="flex aspect-square w-60 cursor-pointer flex-col gap-4 rounded-2xl border-t-[20px] bg-white/75 p-6"
+    :style="'border-top-color: ' + props.station.hexColor"
+    @click="toggleDetail"
   >
     <h6 class="w-full text-center">{{ station.name }}</h6>
     <ul>
@@ -27,10 +37,37 @@ function getTotalBeansEarned(station: Station): number {
       <li>total Beans Earned: {{ station.children.length }}</li>
     </ul>
   </div>
+  <transition name="detail" mode="in-out">
+    <station-detail
+      @close-detail="toggleDetail"
+      :station="station"
+      class="fixed left-0 top-0 z-10 h-screen w-screen overflow-y-auto"
+      v-if="isDetailOpen"
+    />
+  </transition>
 </template>
 
 <style scoped>
-a {
-  background-color: rgba(18, 52, 86, 0.5);
+.detail-enter-active,
+.detail-leave-active {
+  transition:
+    transform 0.3s ease,
+    opacity 0.3s ease;
+}
+
+.detail-enter-from {
+  transform: translateX(100%);
+}
+
+.detail-enter-to {
+  transform: translateX(0);
+}
+
+.detail-leave-from {
+  transform: translateX(0);
+}
+
+.detail-leave-to {
+  transform: translateX(100%);
 }
 </style>
