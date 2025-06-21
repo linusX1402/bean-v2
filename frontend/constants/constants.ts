f4eb95b55e6657aafb7723307cdeab9d66474bdd
linusX1402 <linus@wizany.at>
2025-06-21 14:52:56 +0200

    update: merge constants and types into one file and add more constants for easier testing and maintainability

--- frontend/constants/constants.ts
+++ frontend/constants/constants.ts
@@ -1,6 +1,3 @@
-export const DEFAULT_SECONDS_PER_TICK = 60;
-export const DEFAULT_BEANS_PER_TICK = 5;
-export const DEFAULT_STARTING_FUNDS = 5;
 export const DEFAULT_ICON = '🫘';
 export const ICON_LIST = ['🫘', '🐋', '🪨', '💸'];
 export const PLACEHOLDERS = [
@@ -18,3 +15,21 @@ export const PLACEHOLDERS = [
   "Pasis's party",
   "Tobi's gathering",
 ];
+
+// Beans per tick
+export const DEFAULT_BEANS_PER_TICK = 1;
+export const MIN_BEANS_PER_TICK = 1;
+export const MAX_BEANS_PER_TICK = 50;
+export const BEANS_PER_TICK_STEP_SIZE = 1;
+
+// Seconds per tick
+export const DEFAULT_SECONDS_PER_TICK = 300;
+export const MIN_SECONDS_PER_TICK = 30;
+export const MAX_SECONDS_PER_TICK = 600;
+export const SECONDS_PER_TICK_STEP_SIZE = 30;
+
+// Starting funds
+export const DEFAULT_STARTING_FUNDS = 5;
+export const MIN_STARTING_FUNDS = 0;
+export const MAX_STARTING_FUNDS = 50;
+export const STARTING_FUNDS_STEP_SIZE = 1;
