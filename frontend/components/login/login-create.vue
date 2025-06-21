a3c177fea9afbad99f96645079e97215e555b35f
linusX1402 <linus@wizany.at>
2025-06-21 14:51:12 +0200

    update: use of constants

--- frontend/components/login/login-create.vue
+++ frontend/components/login/login-create.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
-import type BeanSessionDTO from '~/models/bean-session-dto';
 import {
+  BEANS_PER_TICK_STEP_SIZE,
   DEFAULT_BEANS_PER_TICK,
   DEFAULT_ICON,
   DEFAULT_SECONDS_PER_TICK,
   DEFAULT_STARTING_FUNDS,
   ICON_LIST,
+  MAX_BEANS_PER_TICK,
+  MAX_SECONDS_PER_TICK,
+  MAX_STARTING_FUNDS,
+  MIN_BEANS_PER_TICK,
+  MIN_SECONDS_PER_TICK,
+  MIN_STARTING_FUNDS,
+  SECONDS_PER_TICK_STEP_SIZE,
+  STARTING_FUNDS_STEP_SIZE,
 } from '~/constants/constants';
+import Stepper from '~/components/form/stepper.vue';
+import type NewBeanSessionDTO from '~/models/new-bean-session-dto';
 
 type BeanSessionError = {
   name: boolean;
@@ -26,7 +36,9 @@ enum loginViews {
 const props = defineProps<{
   currentView: loginViews;
   doForwardUser: boolean;
+  apiError: boolean;
 }>();
+
 const emit = defineEmits([
   'update:change-view',
   'update:submit-create',
@@ -34,15 +46,11 @@ const emit = defineEmits([
 ]);
 
 const doForwardUserComponent = ref(props.doForwardUser);
-const newSession = ref<BeanSessionDTO>({
+const newSession = ref<NewBeanSessionDTO>({
   name: '',
   icon: DEFAULT_ICON,
-  stations: [],
-  sessionIdAdmin: '',
-  sessionIdEditor: '',
-  sessionIdUser: '',
   secondsPerTick: DEFAULT_SECONDS_PER_TICK,
-  beanPerTick: DEFAULT_BEANS_PER_TICK,
+  beansPerTick: DEFAULT_BEANS_PER_TICK,
   startingFunds: DEFAULT_STARTING_FUNDS,
 });
 
@@ -55,16 +63,88 @@ const newSessionError = ref<BeanSessionError>({
   startingFunds: false,
 });
 
+const scrollDivOnOpen = ref<HTMLElement | undefined>(undefined);
+
 function submitCreate() {
   emit('update:submit-create', newSession.value);
   emit('update:forward-user', doForwardUserComponent.value);
+}
+
+function updateSessionProperty(
+  property: keyof NewBeanSessionDTO,
+  operation: 'add' | 'subtract',
+  value: number = 1,
+) {
+  if (
+    newSession.value[property] !== undefined &&
+    (property === 'beansPerTick' ||
+      property === 'secondsPerTick' ||
+      property === 'startingFunds')
+  ) {
+    if (
+      operation === 'add' &&
+      newSession.value[property] + value >= getMinValue(property) &&
+      newSession.value[property] + value <= getMaxValue(property)
+    ) {
+      newSession.value[property] += value;
+    } else if (
+      operation === 'subtract' &&
+      newSession.value[property] - value >= getMinValue(property) &&
+      newSession.value[property] - value <= getMaxValue(property)
+    ) {
+      newSession.value[property] -= value;
+    }
+  }
+}
+
+function getMaxValue(property: keyof NewBeanSessionDTO): number {
+  switch (property) {
+    case 'beansPerTick':
+      return MAX_BEANS_PER_TICK;
+    case 'secondsPerTick':
+      return MAX_SECONDS_PER_TICK;
+    case 'startingFunds':
+      return MAX_STARTING_FUNDS;
+    default:
+      return -1;
+  }
+}
+
+function getMinValue(property: keyof NewBeanSessionDTO): number {
+  switch (property) {
+    case 'beansPerTick':
+      return MIN_BEANS_PER_TICK;
+    case 'secondsPerTick':
+      return MIN_SECONDS_PER_TICK;
+    case 'startingFunds':
+      return MIN_STARTING_FUNDS;
+    default:
+      return -1;
+  }
+}
+
+const timePerTick = computed(() => {
+  const minutes = Math.floor(newSession.value.secondsPerTick / 60);
+  const seconds = newSession.value.secondsPerTick % 60;
+  return seconds !== 0 ? `${minutes},5 min` : `${minutes} min`;
+});
+
+function scrollToAddStationCard(delay: number = 0) {
+  setTimeout(() => {
+    if (addStationRef.value) {
+      addStationRef.value.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+      });
+    }
+  }, delay);
 }
 </script>
 
 <template>
   <form
     v-if="currentView === loginViews.create"
-    class="flex h-full flex-col place-content-between place-items-center gap-8"
+    class="flex h-fit min-h-full flex-col place-content-between place-items-center gap-8"
     @submit.prevent="submitCreate"
   >
     <div
@@ -74,10 +154,7 @@ function submitCreate() {
       <div class="flex w-full flex-col gap-3">
         <div class="flex w-full flex-col gap-2">
           <div class="w-full">
-            <label class="w-full">Session Name:</label>
-            <label v-if="newSessionError.name" class="text-red-500">
-              Invalid Session name!
-            </label>
+            <label for="sessionId" class="w-full">Session Name:</label>
             <form-text
               :max-length="50"
               v-model="newSession.name"
@@ -85,12 +162,20 @@ function submitCreate() {
               name="sessionId"
               placeholder="Session Name"
             />
+            <label
+              for="sessionId"
+              v-if="newSessionError.name || apiError"
+              class="text-red-500"
+            >
+              Invalid Session name!
+            </label>
           </div>
         </div>
         <div class="flex w-full place-content-start place-items-center gap-2">
-          <label>icon:</label>
+          <label for="icon">icon:</label>
           <div>
             <select
+              name="icon"
               v-model="newSession.icon"
               class="h-fit w-full rounded-md border border-solid border-black/50 p-1"
             >
@@ -98,9 +183,73 @@ function submitCreate() {
             </select>
           </div>
         </div>
+        <div
+          class="flex w-full place-content-between place-items-center md:gap-5"
+        >
+          <label>Beans Per Tick: {{ newSession.beansPerTick }}</label>
+          <stepper
+            @update:add="
+              updateSessionProperty(
+                'beansPerTick',
+                'add',
+                BEANS_PER_TICK_STEP_SIZE,
+              )
+            "
+            @update:subtract="
+              updateSessionProperty(
+                'beansPerTick',
+                'subtract',
+                BEANS_PER_TICK_STEP_SIZE,
+              )
+            "
+          />
+        </div>
+        <div
+          class="flex w-full place-content-between place-items-center md:gap-5"
+        >
+          <label>Starting funds: {{ newSession.startingFunds }}</label>
+          <stepper
+            @update:add="
+              updateSessionProperty(
+                'startingFunds',
+                'add',
+                STARTING_FUNDS_STEP_SIZE,
+              )
+            "
+            @update:subtract="
+              updateSessionProperty(
+                'startingFunds',
+                'subtract',
+                STARTING_FUNDS_STEP_SIZE,
+              )
+            "
+          />
+        </div>
+        <div
+          class="flex w-full place-content-between place-items-center md:gap-5"
+        >
+          <label>Seconds Per Tick: {{ timePerTick }}</label>
+          <stepper
+            @update:add="
+              updateSessionProperty(
+                'secondsPerTick',
+                'add',
+                SECONDS_PER_TICK_STEP_SIZE,
+              )
+            "
+            @update:subtract="
+              updateSessionProperty(
+                'secondsPerTick',
+                'subtract',
+                SECONDS_PER_TICK_STEP_SIZE,
+              )
+            "
+          />
+        </div>
         <div class="flex w-full place-content-start place-items-center gap-2">
-          <label>Stay signed in</label>
+          <label for="stay-signed-in">Stay signed in</label>
           <input
+            name="stay-signed-in"
             class="size-4"
             type="checkbox"
             v-model="doForwardUserComponent"
@@ -119,6 +268,7 @@ function submitCreate() {
       >
       <ui-button :style="'primary'" :type="'submit'">create</ui-button>
     </div>
+    <div ref="scrollDivOnOpen" />
   </form>
 </template>
 
